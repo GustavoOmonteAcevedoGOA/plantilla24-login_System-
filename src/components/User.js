663d775d4ffa031b5aa6e200eb18195ca4ca7aac
Gustavo Omonte Acevedo <GustavoGOAweb@gmail.com>
2021-09-15 11:55:14 -0400

    First Functional component

---
+++ src/components/User.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//functional React component
+
+function User (){
+    return(
+        <div className="container">
+            <h2>User Functional Component</h2>
+        </div>
+    )
+}
+export default User;
